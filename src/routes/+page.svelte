c566f114dda48cc83bf53ea5c51c425d06c8eb1b
safayet404 <hossainsafayet187@gmail.com>
2025-02-08 02:21:00 +0600

    mini game project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1 class="text-4xl text-red-700">Welcome to SvelteKit</h1>
+<p class="text-lg"> Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+
+<button class="btn btn-accent">Acent</button>
